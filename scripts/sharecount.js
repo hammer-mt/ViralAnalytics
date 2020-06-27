5b3afb4aa9067dedeb042492eb15f2f79c9f2563
Michael Taylor <mjt145@gmail.com>
2020-06-27 15:06:48 +0100

    refactored to make pageview an event

--- scripts/sharecount.js
+++ scripts/sharecount.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', function() {
     var vaScript = document.getElementById('vaScript');
-    
+
     var property = vaScript.getAttribute('data-property');
     var userHash = "#"+localStorage['va:userHash'];
 
@@ -19,8 +19,35 @@ window.addEventListener('load', function() {
     // Log error message
     request.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            var share_count = this.responseText;
-            console.log(share_count);
+            var shareCount = this.responseText;
+            shareCount = parseInt(shareCount);
+
+            if (shareCount == 100) {
+                influencerStatus = "Celebrity";
+            } else if (shareCount >= 50) {
+                influencerStatus = "Influencer";
+            } else if (shareCount >= 25) {
+                influencerStatus = "Demi-Influencer";
+            } else if (shareCount >= 10) {
+                influencerStatus = "Micro-Influencer";
+            } else if (shareCount >= 5) {
+                influencerStatus = "Nano-Influencer";
+            } else if (shareCount >= 1) {
+                influencerStatus = "Sharer";
+            } else {
+                influencerStatus = "";
+            }
+
+            window.dataLayer = window.dataLayer || [];
+            window.dataLayer.push({
+                'event': 'va:shareCount',
+                'shareCount': shareCount,
+                'influencerStatus': influencerStatus
+            });
+
+            console.log(shareCount);
+            console.log(influencerStatus);
+
         } else if (this.readyState == 4 && this.status != 200) {
             var error = this.status + ": "+ this.responseText
             console.log(error);
--- scripts/viralanalytics.js
+++ scripts/viralanalytics.js
@@ -21,197 +21,246 @@ exports.vaLoad = (req, res) => {
         * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
         */
 
-        // Set a date for timestamp
-        var date = new Date();
+        // Log an event
+        window.vaEventLog = function(event="pageview", eventMeta="") {
 
-        // Try for local and session storage
-        function storageAvailable(type) {
-            var storage;
-            try {
-                storage = window[type];
-                var x = '__storage_test__';
-                storage.setItem(x, x);
-                storage.removeItem(x);
-                return true;
-            }
-            catch(e) {
-                return e instanceof DOMException && (
-                    // everything except Firefox
-                    e.code === 22 ||
-                    // Firefox
-                    e.code === 1014 ||
-                    // test name field too, because code might not be present
-                    // everything except Firefox
-                    e.name === 'QuotaExceededError' ||
-                    // Firefox
-                    e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
-                    // acknowledge QuotaExceededError only if there's something already stored
-                    (storage && storage.length !== 0);
-            }
-        }
+            // Get property id
+            var vaScript = document.getElementById('vaScript');
+            var property = vaScript.getAttribute('data-property');
 
-        var localStorageCheck = storageAvailable('localStorage');
-        var sessionStorageCheck = storageAvailable('sessionStorage');
-
-        // Check for expiry and remove if so
-        var expire = sessionStorage['va:sessionExpire'];
-        var expiryDate = Date.parse(expire);
-        if (expire && date > expiryDate) {
-            sessionStorage.removeItem("va:sessionExpire");
-            sessionStorage.removeItem("va:pageviewCount");
-            sessionStorage.removeItem("va:sessionHash");
-        }
-
-        // Get user hash from local storage
-        var userHash = localStorage['va:userHash'];
-        var sessionHash = sessionStorage['va:sessionHash'];
-
-        // Get ref hash from URL
-        var refHash = window.location.hash;
-
-        // if there’s no user hash id, make one and store it
-        if(!userHash){
-            var userHash = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-            localStorage['va:userHash'] = userHash;
-        }
-
-        // if there’s no session hash id, make one and store it
-        if(!sessionHash){
-            sessionHash = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-            sessionStorage['va:sessionHash'] = sessionHash;
-            if (refHash != "#"+userHash) {
-                sessionStorage['va:sessionRefHash'] = refHash;
-            }
-            var sessionExpire = new Date(date.getTime() + 30*60000);
-            sessionStorage['va:sessionExpire'] = JSON.stringify(sessionExpire);
-        }
-
-        // Get session and pageview counts
-        var sessionCount = localStorage['va:sessionCount'];
-        var pageviewCount = sessionStorage['va:pageviewCount'];
-
-        // Check session count not null and set if so
-        if(!sessionCount){
-            sessionCount = "0";
-            localStorage['va:sessionCount'] = sessionCount;
-        } else {
-            sessionCount = JSON.parse(sessionCount);
-            sessionCount = JSON.stringify(sessionCount + 1);
-            localStorage['va:sessionCount'] = sessionCount;
-        }
-
-        // Check pageview count not null and set if so
-        if(!pageviewCount){
-            pageviewCount = "0";
-            sessionStorage['va:pageviewCount'] = pageviewCount;
-        } else {
-            pageviewCount = JSON.parse(pageviewCount);
-            pageviewCount = JSON.stringify(pageviewCount + 1);
-            sessionStorage['va:pageviewCount'] = pageviewCount;
-        }
-
-        // Get property id
-        var vaScript = document.getElementById('vaScript');
-        var property = vaScript.getAttribute('data-property');
-
-        // Check for bookmarked posts
-        if (pageviewCount == 0 && refHash == '#'+userHash) {
-            var bookmarkedPost = JSON.stringify(true);
-        } else {
-            var bookmarkedPost = JSON.stringify(false);
-        }
-
-        // Set the session ref and params
-        if (pageviewCount == 0) {
-            sessionStorage['va:sessionReferrer'] = document.referrer;
-            sessionStorage['va:sessionParams'] = window.location.search;
-        }
-        var sessionReferrer = sessionStorage['va:sessionReferrer'];
-        var sessionParams = sessionStorage['va:sessionParams'];
-
-        // Set sessionRefHash
-        var sessionRefHash = sessionStorage['va:sessionRefHash'];
-
-        // Try catch for unsupported APIs
-        try {
-            var height = window.screen.height;
-            var width = window.screen.width;
-            var colorDepth = window.screen.colorDepth;
-        } catch (error) {
-            var height = "";
-            var width = "";
-            var colorDepth = "";
-        }
-        try {
-            var timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-            var locale = Intl.DateTimeFormat().resolvedOptions().locale; 
-        } catch (error) {
-            var timeZone = "";
-            var locale = ""; 
-        }
-        
-        // Get page metadata
-        var data={
-            property: property,
-            userHash: userHash,
-            sessionHash: sessionHash,
-            refHash: refHash,
-            sessionCount: sessionCount,
-            pageviewCount: pageviewCount,
-            href: window.location.href,
-            referrer: document.referrer,
-            title: document.title,
-            userAgent: navigator.userAgent,
-            doNotTrack: navigator.doNotTrack,
-            cookieEnabled: navigator.cookieEnabled,
-            height: height,
-            width: width,
-            colorDepth: colorDepth,
-            timeZone: timeZone,
-            locale: locale,
-            timeZoneOffset: date.getTimezoneOffset(),
-            protocol: window.location.protocol,
-            hostname: window.location.hostname,
-            port: window.location.port,
-            hash: window.location.hash,
-            pathname: window.location.pathname,
-            search: window.location.search,
-            bookmarked: bookmarkedPost,
-            sessionReferrer: sessionReferrer,
-            sessionParams: sessionParams,
-            sessionRefHash: sessionRefHash,
-            localStorageCheck: localStorageCheck,
-            sessionStorageCheck: sessionStorageCheck
-        }
-
-        // Set up API call
-        var url="https://us-central1-genial-core-277717.cloudfunctions.net/va-post";
-
-        var request = new XMLHttpRequest();
-        request.open("POST", url, true);
-        request.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
-        request.send(JSON.stringify(data));
-
-        // Log error message
-        request.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status != 200) {
-                var error = this.status + ": "+ this.responseText
-                console.log(error);
-
-                // Send error to logging function
-                var url="https://us-central1-genial-core-277717.cloudfunctions.net/errorlogger";
-                var payload={
-                    message: error
+            // Try for local and session storage
+            function storageAvailable(type) {
+                var storage;
+                try {
+                    storage = window[type];
+                    var x = '__storage_test__';
+                    storage.setItem(x, x);
+                    storage.removeItem(x);
+                    return true;
                 }
-                var request = new XMLHttpRequest();
-                request.open("POST", url, true);
-                request.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
-                request.send(JSON.stringify(payload));
+                catch(e) {
+                    return e instanceof DOMException && (
+                        // everything except Firefox
+                        e.code === 22 ||
+                        // Firefox
+                        e.code === 1014 ||
+                        // test name field too, because code might not be present
+                        // everything except Firefox
+                        e.name === 'QuotaExceededError' ||
+                        // Firefox
+                        e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+                        // acknowledge QuotaExceededError only if there's something already stored
+                        (storage && storage.length !== 0);
+                }
             }
+
+            // Check local and session storage is accessible
+            var localStorageCheck = storageAvailable('localStorage');
+            var sessionStorageCheck = storageAvailable('sessionStorage');
+
+            // Get user hash from local storage
+            var userHash = localStorage['va:userHash'];
+            var sessionHash = sessionStorage['va:sessionHash'];
+
+            // Try catch for unsupported APIs
+            try {
+                var height = window.screen.height;
+                var width = window.screen.width;
+                var colorDepth = window.screen.colorDepth;
+            } catch (error) {
+                var height = "";
+                var width = "";
+                var colorDepth = "";
+            }
+            try {
+                var timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+                var locale = Intl.DateTimeFormat().resolvedOptions().locale; 
+            } catch (error) {
+                var timeZone = "";
+                var locale = ""; 
+            }
+            // Set a date for timestamp
+            var date = new Date();
+            var timestamp = JSON.stringify(date);
+
+            // Get page meta data
+            var referrer = document.referrer;
+            var search = window.location.search;
+            var href = window.location.href;
+            var title = document.title;
+            var userAgent = navigator.userAgent;
+            var doNotTrack = navigator.doNotTrack;
+            var cookieEnabled = navigator.cookieEnabled;
+            var timeZoneOffset = date.getTimezoneOffset();
+            var protocol = window.location.protocol;
+            var hostname = window.location.hostname;
+            var port = window.location.port;
+            var hash = window.location.hash;
+            var pathname = window.location.pathname;
+
+            // Check for expiry and refresh or remove
+            var expire = sessionStorage['va:sessionExpire'];
+            var expiryDate = Date.parse(expire);
+
+            // If the session exists but has timed out
+            if (expire && date > expiryDate) {
+                sessionStorage.removeItem("va:sessionExpire");
+                sessionStorage.removeItem("va:pageviewCount");
+                sessionStorage.removeItem("va:sessionHash");
+            }
+
+            // Get ref hash from URL
+            var refHash = window.location.hash;
+
+            // if there’s no user hash id, make one and store it
+            if(!userHash){
+                var userHash = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+                localStorage['va:userHash'] = userHash;
+                localStorage['va:userReferrer'] = referrer;
+                localStorage['va:userParams'] = search;
+                localStorage['va:userRefHash'] = refHash;
+            }
+
+            // if there’s no session hash id, make one and store it
+            if(!sessionHash){
+                sessionHash = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+                sessionStorage['va:sessionHash'] = sessionHash;
+                if (refHash != "#"+userHash) {
+                    sessionStorage['va:sessionRefHash'] = refHash;
+                }
+                sessionStorage['va:sessionReferrer'] = referrer;
+                sessionStorage['va:sessionParams'] = search;
+
+                var sessionExpire = new Date(date.getTime() + 30*60000);
+                sessionStorage['va:sessionExpire'] = JSON.stringify(sessionExpire);
+            }
+
+            // Get session and pageview count
+            var sessionCount = localStorage['va:sessionCount'];
+            var pageviewCount = sessionStorage['va:pageviewCount'];
+
+            // Check session count not null and set if so
+            if(!sessionCount){
+                sessionCount = "0";
+                localStorage['va:sessionCount'] = sessionCount;
+            } else {
+                sessionCount = JSON.parse(sessionCount);
+                sessionCount = JSON.stringify(sessionCount + 1);
+                localStorage['va:sessionCount'] = sessionCount;
+            }
+
+            // Get session marketing sources
+            var sessionReferrer = sessionStorage['va:sessionReferrer'];
+            var sessionParams = sessionStorage['va:sessionParams'];
+            var sessionRefHash = sessionStorage['va:sessionRefHash'];
+
+            // Initialize bookmarked post variable
+            var bookmarkedPost = JSON.stringify(false);
+
+            // Initialize event and event meta
+            var eventName = "pageview";
+            var eventParams = "";
+
+            // Check if event is pageview
+            if (event=='pageview') {
+
+                // Check pageview count not null and set if so
+                if(!pageviewCount){
+                    pageviewCount = "0";
+                    sessionStorage['va:pageviewCount'] = pageviewCount;
+                } else {
+                    pageviewCount = JSON.parse(pageviewCount);
+                    pageviewCount = JSON.stringify(pageviewCount + 1);
+                    sessionStorage['va:pageviewCount'] = pageviewCount;
+                }
+
+                // Check for bookmarked posts
+                if (pageviewCount == "0" && refHash == '#'+userHash) {
+                    bookmarkedPost = JSON.stringify(true);
+                }
+
+                
+            // Event is custom, not a pageview
+            } else {
+                // Log event metadata
+                eventName = event;
+                eventParams = JSON.stringify(eventMeta);
+            }
+           
+            // Get page metadata
+            var data={
+                property: property,
+                userHash: userHash,
+                sessionHash: sessionHash,
+                refHash: refHash,
+                sessionCount: sessionCount,
+                pageviewCount: pageviewCount,
+                href: href,
+                referrer: referrer,
+                title: title,
+                userAgent: userAgent,
+                doNotTrack: doNotTrack,
+                cookieEnabled: cookieEnabled,
+                height: height,
+                width: width,
+                colorDepth: colorDepth,
+                timeZone: timeZone,
+                locale: locale,
+                timeZoneOffset: timeZoneOffset,
+                protocol: protocol,
+                hostname: hostname,
+                port: port,
+                hash: hash,
+                pathname: pathname,
+                search: search,
+                bookmarked: bookmarkedPost,
+                sessionReferrer: sessionReferrer,
+                sessionParams: sessionParams,
+                sessionRefHash: sessionRefHash,
+                userReferrer: userReferrer,
+                userParams: userParams,
+                userRefHash: userRefHash,
+                localStorageCheck: localStorageCheck,
+                sessionStorageCheck: sessionStorageCheck,
+                eventName: eventName,
+                eventMeta: eventMeta,
+                timestamp: timestamp
+            }
+
+            // Set up API call
+            var url="https://us-central1-genial-core-277717.cloudfunctions.net/va-post";
+
+            var request = new XMLHttpRequest();
+            request.open("POST", url, true);
+            request.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+            request.send(JSON.stringify(data));
+
+            // Log error message
+            request.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status != 200) {
+                    var error = this.status + ": "+ this.responseText
+                    console.log(error);
+
+                    // Send error to logging function
+                    var url="https://us-central1-genial-core-277717.cloudfunctions.net/errorlogger";
+                    var payload={
+                        message: error
+                    }
+                    var request = new XMLHttpRequest();
+                    request.open("POST", url, true);
+                    request.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+                    request.send(JSON.stringify(payload));
+                }
+            }
+
+            // Change window location hash
+            window.location.hash = userHash;
         }
+        // Fire pageview
+        vaEventLog();
+
+        // Set up event queue
         
-        // Change window location hash
-        window.location.hash = userHash;
     }
     res.send("("+viralAnalyics.toString()+")();")
 };
